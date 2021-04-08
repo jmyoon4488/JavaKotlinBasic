9978a65220696cdf269c4fa1a926bbd8eacf2ace
YJM <jmyoon4488@gmail.com>
2021-04-08 21:14:35 +0900

    [add] 익명함수 / inline / 확장함수 / infix / 고차함수 / 배열

---
+++ src/main/kotlin/Study_40_InFixFunction/InfixFunction.kt
@@ -0,0 +1,24 @@
+package Study_40_InFixFunction
+
+/*
+함수를 연산자처럼 사용할 수 있는 함수입니다.
+* */
+class InfixFunction {
+}
+
+fun main() {
+
+    //      this add a1
+    val r1 = 100 add 50
+
+    val r2 = 100
+    r2.add1(50)
+}
+
+inline fun Int.add1(a1: Int) : Int {
+    return this + a1
+}
+
+infix fun Int.add(a1: Int) : Int {
+    return this + a1
+}
